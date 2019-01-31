ac57ecae1865ac75ee019cb32dab9c047bd989ba
quyuan <quyuan891211@163.com>
2019-01-31 16:43:09 +0800

    Problem35_Array/Solution1.java

---
+++ src/Problem35_Array/Solution1.java
@@ -0,0 +1,57 @@
+package Problem35_Array;
+
+/**
+ * @Author: QuYuan
+ * @Description:35. Search Insert Position
+Easy
+
+1094
+
+156
+
+Favorite
+
+Share
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+ * @Date: Created in 16:04 2019/1/31
+ * @Modified By:
+ */
+public class Solution1 {
+    //    half-interval search
+    public static int searchInsertPosition(Integer[] nums, Integer val) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = (int) (low + high) / 2;
+            if (nums[mid] < val) {
+                low = mid + 1;
+            } else if (nums[mid] > val) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return low;
+    }
+}
+
